8bab6a7f2a8ff743c92beaf2130cd1a512688e6d
Mitanshu Reshamwala <42906426+MitanshuShaBa@users.noreply.github.com>
2020-08-15 22:33:41 +0530

    heart_disease_api.py done

--- app/routers/api.py
+++ app/routers/api.py
@@ -1,19 +1,6 @@
 from fastapi import APIRouter
+from . import heart_disease_api
 
 router = APIRouter()
 
-# @router.post("/scan", status_code=200)
-# async def scan(file: UploadFile = File(...)):
-#     """
-#     It will extract information using OCR and extract relevant information from it giving a json response
-#     """
-#     filename = file.filename
-#     if not filename.endswith('.pdf'):
-#         return JSONResponse({"message": "Upload PDF file"}, status_code=422)
-#     while os.path.exists(filename):
-#         filename = filename.split('.')[-2] + '_1' + '.' + filename.split('.')[-1]
-#     try:
-#         response = scan_invoice(file.file)
-#         return JSONResponse(response)
-#     except Exception as e:
-#         return JSONResponse(content={"error": str(e)}, status_code=500)
+router.include_router(heart_disease_api.api, prefix='/heart_disease', tags=['Heart Disease UCI'])
